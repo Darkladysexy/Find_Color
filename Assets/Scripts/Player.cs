242dce7a012711b9fdedcae68ac965683b6ee116
Darkladysexy <songoduycu@gmail.com>
2025-06-09 22:10:12 +0700

    PLayer

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 playerInput = new Vector2(moveX, moveY).normalized * speed;
+        rb.velocity = playerInput;
+        if (playerInput.x > 0)
+        {
+            spriteRenderer.flipX = false; // Face right
+        }
+        else if (playerInput.x < 0)
+        {
+            spriteRenderer.flipX = true; // Face left
+        }
+        if (playerInput != Vector2.zero || playerInput != Vector2.zero)
+        {
+            animator.SetBool("isRun", true);
+        }
+        else
+        {
+            animator.SetBool("isRun", false);
+        }
+    }
+}
